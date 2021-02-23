d1747012ec8f7b7aa62286f1a6235c993993c434
SullyDupre <sullivandupre@Sullivans-MacBook-Air.local>
2021-02-23 15:21:42 -0700

    first commit

---
+++ LiftingAppVersion2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LiftingAppVersion2
+//
+//  Created by Sullivan Dupre on 8/26/20.
+//  Copyright © 2020 ASU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# LiftingAppVersion2
